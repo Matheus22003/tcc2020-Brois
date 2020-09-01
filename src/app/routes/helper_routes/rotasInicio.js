b266e4a2b62b87ce986263a0222a4ef55dde288a
Matheus de Oliveira Simões <matheussimoes2203@gmail.com>
2020-09-01 11:06:22 -0300

    1.3

--- src/app/routes/helper_routes/rotasInicio.js
+++ src/app/routes/helper_routes/rotasInicio.js
@@ -9,28 +9,27 @@ const bcrypt = require('bcryptjs');
 const { path, passport, io } = require('../../../config/custom-express');
 
 
-// listaAmigos.novo({
-//     id_user: "5f2c27e96f80143d48a175cd",
-//     id_amigo: "5f35624c9669174a68e691fd",
-//     is_confirmed: false,
-//     is_blocked: false,
-//     is_favorite: false
-// })
+
 
 const rotaGet = (req, res) => {
-
     if (res.locals.user) {
         let listAmig = {};
         const user = res.locals.user
         const userId = user._id
         retornaJson.amigosAtivador(userId);
+        listaAmigos.novo({
+            id_user: "5f2c27e96f80143d48a175cd",
+            id_amigo: "5f35624c9669174a68e691fd",
+            is_confirmed: false,
+            is_blocked: false,
+            is_favorite: false
+        })
         setTimeout(() => {
             listAmig = retornaJson.listaAmigos
             res.render("user_general/index", { listaAmigos: listAmig })
             listAmig = {};
         }, 300)
     }
-
     else {
         res.render("user_general/index");
     }
