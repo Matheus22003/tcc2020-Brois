6d847cc878453a0b59602d087d23b2f55a4efb00
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2020-10-27 08:32:44 -0300

    Um pequeno teste

--- src/app/routes/tournament.js
+++ src/app/routes/tournament.js
@@ -6,6 +6,22 @@ const axios = require('axios');
 router.route('/')
     .get((req, res) => {
         res.render('play/tournaments/index');
+
+        axios.get('br1.api.riotgames.com/lor/ranked/v1/leaderboards', {
+            params: {
+                api_key: "RGAPI-7b7c76e0-30f7-4cc2-8863-ca63022f28c1"
+
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
     })
 
 module.exports = router
