c3de255626e900b4fb8e8e82c603380ee919062c
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2020-10-27 11:15:41 -0300

    Update tournament.js

--- src/app/routes/tournament.js
+++ src/app/routes/tournament.js
@@ -7,7 +7,7 @@ router.route('/')
     .get((req, res) => {
 
         axios.post('https://americas.api.riotgames.com/lol/tournament-stub/v4/providers',
-            { params: { api_key: "RGAPI-a28cdd7c-6884-42a4-acb1-7fcfd1c6a3e6" } },
+            { params: { api_key: "RGAPI-7b7c76e0-30f7-4cc2-8863-ca63022f28c1" } },
             { headers: { "region": "BR", "url": "https://tcc2020-brois.herokuapp.com/" } })
             .then(function (response) {
                 res.render('play/tournaments/index');
