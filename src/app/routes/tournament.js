778c4436e55da19e92a92d7f35d1053443d25365
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2020-10-31 03:35:27 -0300

    teste

--- src/app/routes/tournament.js
+++ src/app/routes/tournament.js
@@ -3,10 +3,36 @@ const router = express.Router();
 // const rotasTour = require('./helper_routes/rotasTournament')
 const axios = require('axios');
 const TournamentDao = require(`../infra/tournament-dao`);
+const EquipeDao = require(`../infra/equipe-dao`)
 
 
 router.route('/')
     .get(async (req, res) => {
+        var timesCertos = [];
+        const equipeDao = new EquipeDao()
+        var equipe = await equipeDao.getEquipe()
+        equipe = JSON.stringify(equipe);
+        equipe = JSON.parse(equipe);
+        for (let index = 0; index < equipe.length; index++) {
+            const element = equipe[index];
+            if (element.id_team.id_player1 == res.locals.user._id) {
+                timesCertos.push(element)
+            }
+            if (element.id_team.id_player2 == res.locals.user._id) {
+                timesCertos.push(element)
+            }
+            if (element.id_team.id_player3 == res.locals.user._id) {
+                timesCertos.push(element)
+            }
+            if (element.id_team.id_player4 == res.locals.user._id) {
+                timesCertos.push(element)
+            }
+            if (element.id_team.id_player5 == res.locals.user._id) {
+                timesCertos.push(element)
+            }
+        }
+
+
         var tournamentDao = new TournamentDao();
         var torneios = await tournamentDao.getTorneios();
         torneios = JSON.stringify(torneios);
@@ -21,12 +47,19 @@ router.route('/')
             const element = torneios[index];
 
             if (element.iniciado == false) {
-                torDisponiveis.push(element); 
+                torDisponiveis.push(element);
             }
             else {
 
             }
         }
+        for (let index = 0; index < torDisponiveis.length; index++) {
+            torDisponiveis[index].equipesUsers = timesCertos
+        }
+
+
+
+        // console.log(timesCertos);
         // console.log(torDisponiveis);
 
         res.render('play/tournaments/index', { torneios: torDisponiveis });
